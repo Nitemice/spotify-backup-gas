6f937a9982bed05ef71dbf81869867c28af3d5b1
Nitemice <webmaster@nitemice.net>
2021-09-25 00:39:56 +1000

    Stage 3: Added playlist backup

--- code.js
+++ code.js
@@ -9,6 +9,8 @@ const savedAlbumsUrl = baseUrl + "/me/albums";
 const savedShowsUrl = baseUrl + "/me/shows";
 const savedEpisodesUrl = baseUrl + "/me/episodes";
 
+const playlistsUrl = baseUrl + "/me/playlists";
+
 function getData(accessToken, url, getAllPages = false)
 {
     var headers = {
@@ -105,8 +107,8 @@ function backupFollowing()
     var filename = "following";
     if (config.outputFormat.includes("raw"))
     {
-        var followingData = JSON.stringify(data, null, 4);
-        common.updateOrCreateFile(config.backupDir, filename + ".json", followingData);
+        var output = JSON.stringify(data, null, 4);
+        common.updateOrCreateFile(config.backupDir, filename + ".json", output);
     }
     if (config.outputFormat.includes("csv"))
     {
@@ -141,8 +143,8 @@ function backupSavedTracks()
     var filename = "savedTracks";
     if (config.outputFormat.includes("raw"))
     {
-        var followingData = JSON.stringify(data, null, 4);
-        common.updateOrCreateFile(config.backupDir, filename + ".json", followingData);
+        var output = JSON.stringify(data, null, 4);
+        common.updateOrCreateFile(config.backupDir, filename + ".json", output);
     }
     if (config.outputFormat.includes("csv"))
     {
@@ -154,7 +156,7 @@ function backupSavedTracks()
             track.track.artists.forEach(artist =>
             {
                 line += JSON.stringify(artist.name) + ";";
-    
+
             });
             line = line.slice(0, -1) + ",";
             line += JSON.stringify(track.track.album.name) + ",";
@@ -184,8 +186,8 @@ function backupSavedAlbums()
     var filename = "savedAlbums";
     if (config.outputFormat.includes("raw"))
     {
-        var followingData = JSON.stringify(data, null, 4);
-        common.updateOrCreateFile(config.backupDir, filename + ".json", followingData);
+        var output = JSON.stringify(data, null, 4);
+        common.updateOrCreateFile(config.backupDir, filename + ".json", output);
     }
     if (config.outputFormat.includes("csv"))
     {
@@ -225,8 +227,8 @@ function backupSavedShows()
     var filename = "savedShows";
     if (config.outputFormat.includes("raw"))
     {
-        var followingData = JSON.stringify(data, null, 4);
-        common.updateOrCreateFile(config.backupDir, filename + ".json", followingData);
+        var output = JSON.stringify(data, null, 4);
+        common.updateOrCreateFile(config.backupDir, filename + ".json", output);
     }
     if (config.outputFormat.includes("csv"))
     {
@@ -261,8 +263,8 @@ function backupSavedEpisodes()
     var filename = "savedEpisodes";
     if (config.outputFormat.includes("raw"))
     {
-        var followingData = JSON.stringify(data, null, 4);
-        common.updateOrCreateFile(config.backupDir, filename + ".json", followingData);
+        var output = JSON.stringify(data, null, 4);
+        common.updateOrCreateFile(config.backupDir, filename + ".json", output);
     }
     if (config.outputFormat.includes("csv"))
     {
@@ -289,6 +291,110 @@ function backupSaved()
     backupSavedEpisodes();
 }
 
+function backupPlaylists()
+{
+    // Retrieve auth
+    var accessToken = retrieveAuth();
+
+    // Make a folder for all list files
+    var backupFolder = common.findOrCreateFolder(config.backupDir, "playlists").getId();
+
+    // Retrieve a list of all the lists
+    var params = "?limit=50";
+    var allPlaylists = getData(accessToken, playlistsUrl + params, true);
+    allPlaylists = common.collateArrays("items", allPlaylists);
+
+    // Iterate through the lists and retrieve each one
+    for (list of allPlaylists)
+    {
+        // Retrieve playlist tracks
+        var name = list.name;
+        var tracks = getData(accessToken, list.tracks.href + params, true);
+        tracks = common.collateArrays("items", tracks);
+        list.tracks = tracks;
+
+        if (config.outputFormat.includes("raw"))
+        {
+            // Save the json file in the indicated Google Drive folder
+            var output = JSON.stringify(list, null, 4);
+            common.updateOrCreateFile(backupFolder, name + ".json", output);
+        }
+        if (config.outputFormat.includes("csv"))
+        {
+            var csvData = "name, artist, album, track number, uri, date added\n";
+
+            tracks.forEach(track =>
+            {
+                var line = JSON.stringify(track.track.name) + ",";
+                track.track.artists.forEach(artist =>
+                {
+                    line += JSON.stringify(artist.name) + ";";
+
+                });
+                line = line.slice(0, -1) + ",";
+                line += JSON.stringify(track.track.album.name) + ",";
+                line += track.track.track_number + ",";
+                line += track.track.uri + ",";
+                line += track.added_at + ",";
+                csvData += line + "\n"
+            });
+
+            common.updateOrCreateFile(backupFolder, name + ".csv", csvData);
+        }
+        if (config.outputFormat.includes("xspf"))
+        {
+            var ns = XmlService.getNamespace("http://xspf.org/ns/0/");
+            var root = XmlService.createElement("playlist", ns)
+                .setAttribute("version", "1");
+            root.addContent(
+                XmlService.createElement("creator").setText(list.owner.display_name));
+            root.addContent(
+                XmlService.createElement("annotation").setText(list.description));
+            root.addContent(
+                XmlService.createElement("title").setText(name));
+            root.addContent(
+                XmlService.createElement("location").setText(list.external_urls.spotify));
+            root.addContent(
+                XmlService.createElement("identifier").setText(list.uri));
+
+            var trackList = XmlService.createElement("trackList");
+
+            tracks.forEach(track =>
+            {
+                var trackElement = XmlService.createElement("track");
+                trackElement.addContent(
+                    XmlService.createElement("identifier").setText(track.track.uri));
+                trackElement.addContent(
+                    XmlService.createElement("title").setText(track.track.name));
+
+                var artists = "";
+                track.track.artists.forEach(artist =>
+                {
+                    artists += artist.name + ";";
+
+                });
+                artists = artists.slice(0, -1);
+                trackElement.addContent(
+                    XmlService.createElement("creator").setText(artists));
+
+                trackElement.addContent(
+                    XmlService.createElement("album").setText(track.track.album.name));
+                trackElement.addContent(
+                    XmlService.createElement("trackNum").setText(track.track.track_number));
+                trackElement.addContent(
+                    XmlService.createElement("meta").setAttribute("rel", "date_added").setText(track.added_at));
+
+                trackList.addContent(trackElement);
+            });
+            root.addContent(trackList);
+
+            // Prepare XML for output
+            var document = XmlService.createDocument(root);
+            var output = XmlService.getPrettyFormat().format(document);
+            common.updateOrCreateFile(backupFolder, name + ".xspf", output);
+        }
+    }
+}
 
 function main()
 {
